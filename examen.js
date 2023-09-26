13f4ca1a934c7f2e0b3cf983a328bc56a8193bea
a20160017 <131173376+a20160017@users.noreply.github.com>
2023-09-26 08:48:08 -0500

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,13 @@
+var nom=prompt ("Ingrese su nombre") 
+alert ("Bienvenidos")
+var preg1=prompt("¿Cuánta agua hay en la Tierra?\nA:Alrededor de un 41% de la superficie terrestre es agua\nB:Alrededor de un 51% de la superficie terrestre es agua\nC:Alrededor de un 71% de la superficie terrestre es agua")
+if (preg1=="B"){
+    preg1=1
+}
+var preg2=prompt("¿Qué océano es el más grande?\nA:El Pacífico\nB:El Atlántico\nC:El Índico")
+if (preg2=="A"){
+    preg2=1
+}
+var preg3=prompt("¿Cuánto mide la parte más profunda de los Océanos?\nA:11 Km\nB:23 Km\nC:31 Km")
+if (preg3=="C")
+    preg3=1
